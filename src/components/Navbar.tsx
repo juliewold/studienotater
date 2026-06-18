e0c34c28ec43cb425e10d5fae51267b221609961
Julie Wold <juliewold2006@gmail.com>
2026-06-18 13:36:25 +0200

    Style initial navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,17 @@
 export const Navbar = () => {
   return (
-    <nav>
-      <h1>Studienotater</h1>
-    </nav>
+    <header className="navbar">
+      <a href="#" className="logo">
+        Studienotater
+      </a>
+      <nav className="nav-links">
+        <a href="#">Hjem</a>
+        <a href="#">Fag</a>
+        <a href="#">Notater</a>
+        <a href="#">Flashcards</a>
+      </nav>
+
+      <button className="menu-button">☰</button>
+    </header>
   );
 };
