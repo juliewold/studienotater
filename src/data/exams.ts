3f10e3a7f457351ad89320884ed210db848042f4
Julie Wold <juliewold2006@gmail.com>
2026-06-18 17:01:02 +0200

    Add exams page and exam data

---
+++ src/data/exams.ts
@@ -0,0 +1,16 @@
+export const exams = {
+  tma4412: [
+    {
+      id: "vaar-2025",
+      title: "Vår 2025",
+      pdf: "#",
+      solution: "#",
+    },
+    {
+      id: "host-2024",
+      title: "Høst 2024",
+      pdf: "#",
+      solution: "#",
+    },
+  ],
+};
