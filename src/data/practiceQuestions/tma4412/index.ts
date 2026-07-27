68750fecdf4c4438e54d9468dd85692634fff011
Julie Wold <juliewold2006@gmail.com>
2026-07-27 14:29:47 +0200

    feat: add modular inverse practice questions

--- src/data/practiceQuestions/tma4412/index.ts
+++ src/data/practiceQuestions/tma4412/index.ts
@@ -14,6 +14,7 @@ import { rekursjonQuestions } from "./rekursjon";
 import { induktivtDefinerteMengderQuestions } from "./induktivtDefinerteMengder";
 import { delbarhetOgPrimtallQuestions } from "./delbarhetOgPrimtall";
 import { diofantiskeLigningerQuestions } from "./diofantiskeLigninger";
+import { modulaereInverserQuestions } from "./modulaereInverser";
 
 export const tma4412Questions: PracticeQuestion[] = [
   ...mengderQuestions,
@@ -31,4 +32,5 @@ export const tma4412Questions: PracticeQuestion[] = [
   ...induktivtDefinerteMengderQuestions,
   ...delbarhetOgPrimtallQuestions,
   ...diofantiskeLigningerQuestions,
+  ...modulaereInverserQuestions,
 ];
